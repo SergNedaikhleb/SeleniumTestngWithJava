afbaefac710f18d0ef2c747d501b5a5cba8e112d
Недайхлеб Сергей <nedajhleb@pup.local>
2019-12-03 12:42:53 +0200

    parallel

---
+++ src/test/java/ParallelExecution/BaseClass.java
@@ -0,0 +1,38 @@
+package ParallelExecution;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseClass {
+    public static ThreadLocal<WebDriver> dr = new ThreadLocal<WebDriver>();
+
+    @BeforeMethod
+    public void beforemethod() throws IllegalAccessException{
+
+        WebDriver driver = new WebDriverFactory().create("Firefox");
+        setWebDriver(driver);
+
+
+        getWebDriver().manage().window().maximize();
+        getWebDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
+    public void setWebDriver(WebDriver driver){
+        dr.set(driver);
+    }
+
+
+    public WebDriver getWebDriver(){
+        return dr.get();
+    }
+
+    @AfterMethod
+    public void aftermethod(){
+        getWebDriver().quit();
+        dr.set(null);
+    }
+}
