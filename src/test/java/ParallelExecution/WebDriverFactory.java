c3b66b057546209e7c4ad9bb58f887f1ccc7d402
Недайхлеб Сергей <nedajhleb@rozetka.com.ua>
2020-02-20 16:47:03 +0200

    3

--- src/test/java/ParallelExecution/WebDriverFactory.java
+++ src/test/java/ParallelExecution/WebDriverFactory.java
@@ -6,21 +6,21 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverFactory {
 
-    static WebDriver create(String type) throws IllegalAccessException{
-        WebDriver driver;
-
-        switch(type) {
-
-            case "Firefox":
-                driver = new FirefoxDriver();
-                break;
-            case "Chrome":
-                driver = new ChromeDriver();
-                break;
-            default:
-                throw new IllegalAccessException();
-        }
-
-        return driver;
-    }
+//    static WebDriver create(String type) throws IllegalAccessException{
+//        WebDriver driver;
+//
+//        switch(type) {
+//
+//            case "Firefox":
+//                driver = new FirefoxDriver();
+//                break;
+//            case "Chrome":
+//                driver = new ChromeDriver();
+//                break;
+//            default:
+//                throw new IllegalAccessException();
+//        }
+//
+//        return driver;
+//    }
 }
